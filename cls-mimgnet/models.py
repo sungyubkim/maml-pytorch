ecd3ceee9a5650e12ab14d86b81001b52765f884
sungyubkim <sungyub.kim@kaist.ac.kr>
2019-08-12 15:14:07 +0900

    fix argparse

--- cls-mimgnet/models.py
+++ cls-mimgnet/models.py
@@ -90,8 +90,7 @@ class DenseNet(nn.Module):
         self.block_size = args.block_size
         self.layers = nn.ParameterDict(OrderedDict([]))
 
-        self.drop_block_1 = DropBlock2D(block_size=1, drop_prob=0.1)
-        self.drop_block_5 = DropBlock2D(block_size=5, drop_prob=0.1)
+        self.drop_block = DropBlock2D(block_size=3, drop_prob=0.1)
 
         # add init conv block
         self.layers.update(
@@ -183,7 +182,7 @@ class DenseNet(nn.Module):
         weight=params['layers.conv_weight'],
         bias=params['layers.conv_bias'])
         x = F.max_pool2d(x, kernel_size=3, stride=2, padding=0)
-        x = self.drop_block_1(x)
+        x = self.drop_block(x)
 
         # apply dense blocks
         for i in range(self.n_block):
@@ -226,7 +225,7 @@ class DenseNet(nn.Module):
             bias=params['layers.conv_transition_{}_bias'.format(i)],
             padding=1)
             x = F.avg_pool2d(x, kernel_size=2, stride=2, padding=0)
-            x = self.drop_block_5(x)
+            x = self.drop_block(x)
 
         x = x.view(-1, x.shape[1] * 6 * 6)
 
